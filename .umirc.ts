66f3c0e38a2975d2f3df67a447f92b697c15fb81
yong <zhengyong@bsoft.com.cn>
2023-01-06 10:58:18 +0800

    dev:slate 配置等

--- .umirc.ts
+++ .umirc.ts
@@ -1,34 +1,45 @@
-import { defineConfig } from '@umijs/max';
+import {defineConfig} from '@umijs/max';
 
 export default defineConfig({
-  antd: {},
-  access: {},
-  model: {},
-  initialState: {},
-  request: {},
-  layout: {
-    title: '@umijs/max',
-  },
-  routes: [
-    {
-      path: '/',
-      redirect: '/home',
-    },
-    {
-      name: '首页',
-      path: '/home',
-      component: './Home',
-    },
-    {
-      name: '权限演示',
-      path: '/access',
-      component: './Access',
-    },
-    {
-      name: ' CRUD 示例',
-      path: '/table',
-      component: './Table',
-    },
-  ],
-  npmClient: 'npm',
+    antd: {},
+    access: {},
+    model: {},
+    initialState: {},
+    request: {},
+    hash: true,
+    layout:
+        // process.env.NODE_ENV === 'development'
+        //     ?
+            {
+                title: '@umijs/max',
+                locale: false, // 默认开启，如无需菜单国际化可关闭
+            },
+            // : false,
+    routes: [
+        {
+            path: '/',
+            redirect: '/home',
+        },
+        {
+            name: '首页',
+            path: '/home',
+            component: './Home',
+        },
+        {
+            name: '权限演示',
+            path: '/access',
+            component: './Access',
+        },
+        {
+            name: ' CRUD 示例',
+            path: '/table',
+            component: './Table',
+        },
+        {
+            name: ' Slate编辑器',
+            path: '/blogEdit',
+            component: './SlateEditor',
+        },
+    ],
+    npmClient: 'npm',
 });
---
+++ src/pages/SlateEditor/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const  SlateEditor :React.FC=()=> {
+
+
+
+  return (
+   <div>
+     Slate编辑器
+   </div>
+  );
+}
+
+export default SlateEditor;
